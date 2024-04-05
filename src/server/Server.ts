1d23c6481eff6371db1656931d7d78fd8d75dbab
ruth.moreira <ruth.moreira@funcionalcorp.com.br>
2024-04-05 09:15:57 -0300

    feat: adicionando variaveis de ambiente e configurando tsconfig para gerar a pasta build

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-
+import 'dotenv/config';
+import { router } from './routes';
 
 const server = express();
 
+server.use(express.json());
 
-server.get('/', (_, res) => {
-  return res.send('Ola, tudo bom?');
-});
+server.use(router);
 
 
 export { server };
