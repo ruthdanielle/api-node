3179343091d8840defd652409ff6616a33ea7e9d
ruth.moreira <ruth.moreira@funcionalcorp.com.br>
2024-04-10 09:31:08 -0300

    feat: add validations on create cidades and yup translations

--- src/server/controllers/cidades/Cidades.ts
+++ src/server/controllers/cidades/Cidades.ts
@@ -1,14 +1,34 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
 
 interface ICidade {
-    nome: string;
+    nome?: string;
 }
 
-export const create = (req: Request<{}, {}, ICidade >, res: Response) => {
-    
-    const data: ICidade = req.body;
+const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
+});
 
-    console.log(data.nome);
+export const create = async (req: Request<{}, {}, ICidade >, res: Response) => {
+    let validatedData: ICidade | undefined = undefined;
+
+    try {
+        validatedData = await bodyValidation.validate(req.body, {abortEarly: false});
+    } catch (err) {
+        const yupError = err as yup.ValidationError;
+        const errors: Record<string, string> = {};
+
+        yupError.inner.forEach(error => {
+            if (!error.path) return;
+
+            errors[error.path] = error.message;
+        });
+
+        return res.status(StatusCodes.BAD_REQUEST).json({errors});
+    }
+
+    console.log(validatedData);
     
     return res.send('Created!') ;
 
