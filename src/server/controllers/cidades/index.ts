90b435a519f513b12101b0d07dfeb8716818d2b1
ruth.moreira <ruth.moreira@funcionalcorp.com.br>
2024-04-16 16:02:04 -0300

    feat: add controllers update, getbyid and delete

--- src/server/controllers/cidades/index.ts
+++ src/server/controllers/cidades/index.ts
@@ -1,7 +1,12 @@
 import * as create from './Create';
 import * as getAll from './GetAll';
-
+import * as getById from './GetById';
+import * as updateById from './UpdateById';
+import * as deleteById from './DeleteById';
 export const CidadesController = {
     ...create,
-    ...getAll
+    ...getAll,
+    ...getById,
+    ...updateById,
+    ...deleteById
 };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,6 +8,9 @@ router.get('/', (_, res) => {
 });
 
 router.get('/cidades', CidadesController.getAllValidation, CidadesController.getAll);
+router.get('/cidades/:id', CidadesController.getByIdValidation, CidadesController.getById);
 router.post('/cidades', CidadesController.createValidation, CidadesController.create);
+router.put('/cidades/:id', CidadesController.updateByIdValidation, CidadesController.updateById);
+router.delete('/cidades/:id', CidadesController.deleteValidation, CidadesController.deleteById);
 
 export {router};
